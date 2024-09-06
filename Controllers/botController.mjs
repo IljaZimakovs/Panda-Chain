3124b43d6eeb4da11e292d21c87567dd39657edf
lightningcraft-0201 <dev.rex.0201@gmail.com>
2024-09-06 14:59:28 +0100

    updated backend for the issues

--- Controllers/botController.mjs
+++ Controllers/botController.mjs
@@ -3,25 +3,11 @@ import Sequence from "../models/sequenceModal.mjs";
 import DailyCheck from "../models/dailyCheckModal.mjs";
 import { Bot, InlineKeyboard } from "grammy";
 import { getNextSequenceValue } from "../utils/sequence.mjs";
+
 const startTGBot = () => {
+  const paidUsers = new Map();
   const bot = new Bot(process.env.TELEGRAM_BOT_API_KEY);
-
-  const photoUrl = 'https://gateway.pinata.cloud/ipfs/QmaEPwtmmPz3CGP7VrgDMgQ9ZinwPHxSc7UAUw9LwkohzY';
-
-  async function checkSubscription(userId) {
-    const chatMember = await bot.api.getChatMember('@PandaChainAnn', userId)
-    console.log(chatMember)
-    const status = chatMember.status;
-
-
-    if (status === 'member' || status === 'administrator' || status === 'creator') {
-      console.log('User is subscribed');
-      return true;
-    } else {
-      console.log('User is not subscribed');
-      return false;
-    }
-  }
+  const photoUrl = 'https://gateway.pinata.cloud/ipfs/QmWB8zcwwgJd6UZVA5ouLH6mY3ZuT32Bse65UDEVbiuUFm';
 
   bot.on("message:text", async (ctx) => {
     const messageText = ctx.message.text;
